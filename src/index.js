639722e829b043b35f462aa88b84008b7bdb4217
rkterungwa16 <terungwakombol@gmail.com>
2020-03-17 01:36:35 +0100

    add more tests

--- src/index.js
+++ src/index.js
@@ -6,51 +6,51 @@ const TYPE_REGEXP = /^ *([A-Za-z0-9][A-Za-z0-9!#$&^_-]{0,126})\/([A-Za-z0-9][A-Z
  */
 class MediaType {
     /**
-     *
-     * @param {*} type
-     * @param {*} subtype
-     * @param {*} suffix
+     * @param {*} void
      */
-    constructor (type, subtype, suffix) {
+    constructor () {
+        this.type = "";
+        this.subtype = "";
+        this.suffix = "";
+    }
+
+    /**
+     * Parse media type to object.
+     * Object will contain type, subtype and suffix as per RFC6838 specification
+     *
+     * @param {string} string
+     * @public
+     *
+     * @return {void} Media type
+     */
+    parse (string) {
+        if (!string) {
+            throw new TypeError("argument string is required");
+        }
+
+        if (typeof string !== "string") {
+            throw new TypeError("argument string is required to be a string");
+        }
+
+        const match = TYPE_REGEXP.exec(string.toLowerCase());
+        if (!match) {
+            throw new TypeError("invalid media type");
+        }
+
+        const type = match[1];
+        let subtype = match[2];
+        let suffix;
+
+        const index = subtype.lastIndexOf("+");
+        if (index !== -1) {
+            suffix = subtype.substr(index + 1);
+            subtype = subtype.substr(0, index);
+        }
+
         this.type = type;
         this.subtype = subtype;
         this.suffix = suffix;
     }
 }
 
-/**
- * Parse media type to object.
- *
- * @param {string} string
- * @return {object}
- * @public
- *
- * @return {MediaType} Media type
- */
-export function parse (string) {
-    if (!string) {
-        throw new TypeError("argument string is required");
-    }
-
-    if (typeof string !== "string") {
-        throw new TypeError("argument string is required to be a string");
-    }
-
-    const match = TYPE_REGEXP.exec(string.toLowerCase());
-
-    if (!match) {
-        throw new TypeError("invalid media type");
-    }
-
-    const type = match[1];
-    let subtype = match[2];
-    let suffix;
-
-    const index = subtype.lastIndexOf("+");
-    if (index !== -1) {
-        suffix = subtype.substr(index + 1);
-        subtype = subtype.substr(0, index);
-    }
-
-    return new MediaType(type, subtype, suffix);
-}
+export const mediatype = new MediaType();
--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,55 @@
 import assert from "assert";
 import { describe, it } from "mocha";
-import { parse } from "../src";
+import { mediatype } from "../src";
+
+const invalidTypes = [
+    " ",
+    "null",
+    "undefined",
+    "/",
+    "text/;plain",
+    "text/\"plain\"",
+    "text/p£ain",
+    "text/(plain)",
+    "text/@plain",
+    "text/plain,wrong"
+  ];
 
 describe("Parse", () => {
-    it("should parse basic type", () => {
-        const type = parse("text/html");
-        console.log("typer --->>>>", type);
-        assert.strictEqual(type.type, "text");
-        assert.strictEqual(type.subtype, "html");
+    it("should parse text media type with html subtype", () => {
+        mediatype.parse("text/html");
+        assert.strictEqual(mediatype.type, "text");
+        assert.strictEqual(mediatype.subtype, "html");
+    });
+
+    it("should parse image media type with subtype svg and suffix xml", () => {
+        mediatype.parse("image/svg+xml");
+        assert.strictEqual(mediatype.type, "image");
+        assert.strictEqual(mediatype.subtype, "svg");
+        assert.strictEqual(mediatype.suffix, "xml");
+    });
+
+    it("should lower-case type", () => {
+        mediatype.parse("IMAGE/SVG+XML");
+        assert.strictEqual(mediatype.type, "image");
+        assert.strictEqual(mediatype.subtype, "svg");
+        assert.strictEqual(mediatype.suffix, "xml");
+    });
+
+    invalidTypes.forEach((type) => {
+        it(`should throw on invalid media type ${JSON.stringify(type)}`, () => {
+            assert.throws(mediatype.parse.bind(null, type), /invalid media type/);
+        });
+    });
+
+    it("should require argument", () => {
+        const requiredString = () => {
+            mediatype.parse();
+        };
+        assert.throws(requiredString, new TypeError("argument string is required"));
+    });
+
+    it("should reject non-strings", () => {
+        assert.throws(mediatype.parse.bind(null, 7), /string.*required/);
     });
 });
